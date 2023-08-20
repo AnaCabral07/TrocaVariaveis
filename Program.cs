30eae1661c1dfd97fc08cf92a1248a680108ad93
Ana Clara <4n4.informatica@gmail.com>
2023-08-20 12:14:32 -0300

    Finaliza o programa Troca de Valores

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string  nome1, nome2, auxiliar;
+
+Console.WriteLine("--- Troca de valores ---\n");
+
+Console.Write("nome1..:");
+nome1 = Console.ReadLine()!;
+
+Console.Write("nome2..:");
+nome2 = Console.ReadLine()!;
+
+auxiliar = nome1;
+nome1 = nome2;
+nome2 = auxiliar;
+
+Console.WriteLine("\nApós trocar os valores temos:\n");
+
+Console.WriteLine($"nome 1 = {nome1}");
+Console.WriteLine($"nome 2 = {nome2}");
